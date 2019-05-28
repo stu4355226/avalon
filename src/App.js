bfb0b87fe034f6fb0d858e3ef242fc1c6424b9cd
cclee-cellnetix <36866348+cclee-cellnetix@users.noreply.github.com>
2019-05-28 15:53:37 -0700

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DenseAppBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <DenseAppBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
