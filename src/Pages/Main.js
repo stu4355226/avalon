a95293f48cb3642d17dfccdfbe35ecac8bc955cf
ChiaCheLee <chiachelee@CHIACHELEE.local>
2019-06-07 15:15:15 -0700

    add lunch.json

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,17 +1,8 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
-import Avatar from "@material-ui/core/Avatar";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 import "./Main.css";
 
 export default class Main extends Component {
